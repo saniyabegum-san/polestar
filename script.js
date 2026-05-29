18253eb2b17a4f5c5f84484d0cf5fc57c297e539
saniyabegum-san <saniyabegum63096@gmail.com>
2026-05-30 03:43:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-import { db } from "./firebase-config.js";
-
-import {
-addDoc,
-collection
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+// import { db } from "./firebase-config.js";
+// import {
+// addDoc,
+// collection
+// } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 function checkPassword(){
 
@@ -81,8 +80,8 @@ checkPassword();
 
 /* Submit all answers */
 
-const submitBtn =
-document.getElementById("submitAnswers");
+/*const submitBtn =
+document.getElementById("submitAnswers");*/
 
 if(submitBtn){
 
